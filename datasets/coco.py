c332daf98b8dad76ca9286f34c333f07ff699ab5
zhou9878 <zhou9878@uni.sydney.edu.au>
2021-04-20 15:42:17 +0800

    add coco2014

--- datasets/coco.py
+++ datasets/coco.py
@@ -157,6 +157,19 @@ def build(image_set, args):
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
 
+def build_coco2014(image_set, args):
+    root = Path(args.coco_path)
+    assert root.exists(), f'provided COCO path {root} does not exist'
+    mode = 'instances'
+    PATHS = {
+         "train": (root / "train2014", root / "annotations" / f'{mode}_train2014.json'),
+        "val": (root / "val2014", root / "annotations" / f'{mode}_val2014.json'),
+    }
+
+    img_folder, ann_file = PATHS[image_set]
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
+    return dataset
+
 def build_hico(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided COCO path {root} does not exist'
