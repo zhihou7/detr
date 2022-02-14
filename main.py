fcca499f5c63f5583fd53df1607de7a0c4ef86cf
houzhi <houzhi91@gmail.com>
2022-02-14 17:59:46 +0800

    main

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def main(args):
 
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=False)
         model_without_ddp = model.module
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
